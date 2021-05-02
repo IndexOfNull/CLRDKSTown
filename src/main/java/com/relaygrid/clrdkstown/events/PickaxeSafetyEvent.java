a23b8868dfa95ee99027fa7bb505b6f5f95af6ac
IndexOfNull <21084775+IndexOfNull@users.noreply.github.com>
2021-05-02 00:24:24 -0500

    protect against unauthorized item pickups

--- src/main/java/com/relaygrid/clrdkstown/events/PickaxeSafetyEvent.java
+++ src/main/java/com/relaygrid/clrdkstown/events/PickaxeSafetyEvent.java
@@ -1,12 +1,14 @@
 package com.relaygrid.clrdkstown.events;
 
+import java.util.UUID;
+
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
@@ -18,16 +20,13 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 import com.relaygrid.clrdkstown.CLRDKSTown;
+import com.relaygrid.clrdkstown.Keys;
 
 public class PickaxeSafetyEvent implements Listener {
-	
-	private static boolean hasNoDropTag(Item item) {
-		return playerCanDropItem(item.getItemStack());
-	}
-	
-	private static boolean playerCanDropItem(ItemStack itemStack) {
+		
+	private static boolean hasNoDropTag(ItemStack itemStack) {
 		try {
-			NamespacedKey key = new NamespacedKey(CLRDKSTown.getInstance(), "canBeDropped");
+			NamespacedKey key = new NamespacedKey(CLRDKSTown.getInstance(), Keys.CAN_BE_DROPPED);
 			ItemMeta itemMeta = itemStack.getItemMeta();
 			PersistentDataContainer container = itemMeta.getPersistentDataContainer();
 
@@ -42,8 +41,34 @@ public class PickaxeSafetyEvent implements Listener {
 		}
 	}
 	
+	private static UUID getItemOwnerUUID(ItemStack itemStack) {
+		try {
+			NamespacedKey key = new NamespacedKey(CLRDKSTown.getInstance(), Keys.ITEM_OWNER);
+			ItemMeta itemMeta = itemStack.getItemMeta();
+			PersistentDataContainer container = itemMeta.getPersistentDataContainer();
+
+			if (container.has(key, PersistentDataType.STRING)) {
+				return UUID.fromString(container.get(key, PersistentDataType.STRING));
+			}
+			return null;
+		} catch (NullPointerException e) {
+			return null;
+		}
+	}
+	
 	private static boolean playerCanDropItem(Player player, ItemStack itemStack) {
-		return player.hasPermission("clrdkstown.dropbypass") || !playerCanDropItem(itemStack);
+		return player.hasPermission("clrdkstown.dropbypass") || !hasNoDropTag(itemStack);
+	}
+	
+	private static boolean playerCanPickupItem(Player player, ItemStack itemStack) {
+		if (player.hasPermission("clrdkstown.ownerbypass")) {
+			return true;
+		}
+		UUID ownerUUID = getItemOwnerUUID(itemStack);
+		if (ownerUUID != null && !player.getUniqueId().equals(ownerUUID)) {
+			return false;
+		}
+		return true;
 	}
 	
 	@EventHandler
@@ -62,16 +87,30 @@ public class PickaxeSafetyEvent implements Listener {
 		Player player = (Player) e.getWhoClicked();
 		Inventory playerInv = player.getInventory();
 		
-		if (!playerCanDropItem(player, cursorItem)) { //Manually moving the item
-			if (!clickedInv.equals(playerInv)) {
+		if (!playerCanDropItem(player, cursorItem) && !clickedInv.equals(playerInv)) { //Manually moving the item
 				e.setCancelled(true);
-			}
 		}
 		
 		if (!playerCanDropItem(player, slotItem) && e.isShiftClick()) { //Shift clicking
 			e.setCancelled(true);
 		}
 		
+		if (!playerCanPickupItem(player, slotItem) && !clickedInv.equals(playerInv)) { //Clicking the item if it's not in the player inv and they don't own it
+			e.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onItemPickup(EntityPickupItemEvent e) {
+		
+		if (!(e.getEntity() instanceof Player)) {
+			return;
+		}
+		
+		Player player = (Player) e.getEntity();
+		if (!playerCanPickupItem(player, e.getItem().getItemStack())) {
+			e.setCancelled(true);
+		}
 	}
 	
 	// Ensures drag events are properly trapped
@@ -92,7 +131,7 @@ public class PickaxeSafetyEvent implements Listener {
 			if (!playerCanDropItem(player, cursor) && inventory.firstEmpty() == -1) {
 				for (int slot = 0; slot < 36; slot++) { //0->35 are main inventory slots
 					ItemStack item = inventory.getItem(slot);
-					if (!playerCanDropItem(item)) {
+					if (!playerCanDropItem(player, item)) {
 						player.getWorld().dropItemNaturally(player.getLocation(), item);
 						inventory.setItem(slot, cursor);
 						e.getView().setCursor(null);
